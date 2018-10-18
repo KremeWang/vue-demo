464cf26889f1f17d811f4e7fc44f09274af1e953
kreme <1401005429@qq.com>
2018-10-18 10:57:50 +0800

    初始化vue项目文件

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <mt-spinner type="snake"></mt-spinner>
+    <mt-spinner type="double-bounce"></mt-spinner>
+    <mt-spinner type="triple-bounce"></mt-spinner>
+    <mt-spinner type="fading-circle"></mt-spinner>
+
+    <!-- 或者接受传入类型的序号 -->
+    <mt-spinner :type="0"></mt-spinner>
+    <mt-spinner :type="1"></mt-spinner>
+    <mt-spinner :type="2"></mt-spinner>
+    <mt-spinner :type="3"></mt-spinner>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,36 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+//在vue项目中引入mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
+
+//在vue项目中引入 mui 
+import './assets/mui/css/mui.min.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+//所有的脚手架项目总的路由在 router /index.js中配置
+//每一个 .vue 的文件都是一个组件 --> 每一个 .vue 的template 中只能有一个根元素
+//main.js 就是我们项目的Js 的入口文件，你想要的所有组件中使用的一些都要在main.js中引入
+//app.vue 是根组件
+
+
+//使用mint-ui 的步骤
+//1.npm i mint-ui -S
+//2.import MintUI from 'mint-ui'
+//3.import 'mint-ui/lib/style.css'
+//4.import App from './App.vue
+
