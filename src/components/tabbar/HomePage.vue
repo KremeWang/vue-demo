021fca24eeac582c8cb33638ae3d5f29d23a5170
kreme <1401005429@qq.com>
2018-10-18 16:06:31 +0800

    Home页轮播图样式

--- src/components/tabbar/HomePage.vue
+++ src/components/tabbar/HomePage.vue
@@ -20,9 +20,19 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  font-weight: normal;
-  text-align: center;
+<style scoped lang="scss">
+.mint-swipe {
+  height: 200px;
+  .mint-swipe-item {
+    &:nth-child(1) {
+      background-color: cyan;
+    }
+    &:nth-child(2) {
+      background-color: #ff3040;
+    }
+    &:nth-child(3) {
+      background-color: skyblue;
+    }
+  }
 }
 </style>
